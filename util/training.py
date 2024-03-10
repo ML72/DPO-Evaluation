a0489af61a7f7f64c8d3f9397d2eb34ca5e507ba
Michael <michael.mingdao.li@gmail.com>
2024-03-10 00:26:16 -0800

    inc max length

--- util/training.py
+++ util/training.py
@@ -52,7 +52,7 @@ class ScriptArguments:
     lora_r: Optional[int] = field(default=8, metadata={"help": "the lora r parameter"})
 
     max_prompt_length: Optional[int] = field(default=1024, metadata={"help": "the maximum prompt length"})
-    max_length: Optional[int] = field(default=1024, metadata={"help": "the maximum sequence length"})
+    max_length: Optional[int] = field(default=1032, metadata={"help": "the maximum sequence length"})
     max_steps: Optional[int] = field(default=1000, metadata={"help": "max number of training steps"})
     logging_steps: Optional[int] = field(default=10, metadata={"help": "the logging frequency"})
     save_steps: Optional[int] = field(default=100, metadata={"help": "the saving frequency"})
